64c524ca4a8df321d31ae1269ddc7ca8be21a03b
esuazo2601 <esteban.suazo.m2601@gmail.com>
2024-06-08 16:52:39 -0400

    docker in progress

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = 3000;
+
+// Conexión a MongoDB
+mongoose.connect('mongodb://mongo:27017/mydatabase', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+    res.send('Hello from Express!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
