42062215da692db55c55bcbc972f86709cd91377
esuazo2601 <esteban.suazo.m2601@gmail.com>
2024-06-09 00:34:59 -0400

    docker files listas

---
+++ client/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  
+  base: "/",
+  plugins: [react()],
+  server: {
+    port: 3001,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:3001",
+    },
+  });
--- server/server.js
+++ server/server.js
@@ -5,12 +5,12 @@ const app = express();
 const port = 3000;
 
 // Conexión a MongoDB
-mongoose.connect('mongodb://mongo:27017/mydatabase', { useNewUrlParser: true, useUnifiedTopology: true })
+/* mongoose.connect('mongodb://mongo:27017/mydatabase', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
-
+ */
 app.get('/', (req, res) => {
-    res.send('Hello from Express!');
+    res.send('Hello from dido!');
 });
 
 app.listen(port, () => {
