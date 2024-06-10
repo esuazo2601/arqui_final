cf5baea42b5e9d03f076453b5bca3424a52926d2
esuazo2601 <esteban.suazo.m2601@gmail.com>
2024-06-09 23:56:58 -0400

    TAMOS READY PAL MAMBOOOO

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> testeando otra vez
+          Edit <code>src/App.jsx</code> testeando
         </p>
       </div>
       <p className="read-the-docs">
--- server/server.js
+++ server/server.js
@@ -4,15 +4,29 @@ const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 
+const mensaje = mongoose.model('Mensaje', new mongoose.Schema({
+    Texto: String,
+    fechaHora: String,
+    Sistema: String,
+    estado: Number
+}))
+
+
 // Conexión a MongoDB
-/* mongoose.connect('mongodb://mongo:27017/mydatabase', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://arqui:1221@mongo:27017/arqui?authSource=admin')
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
- */
+
 app.get('/', (req, res) => {
-    res.send('Hello from dido!');
+    res.send('Hello holi!');
 });
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+app.get('/crear', async (_req, res) => {
+    console.log('creando...')
+    await mensaje.create({Texto: "Prueba",fechaHora:"10-06-2024",Sistema:"Rest", estado: 0})
+    return res.send('ok')
+  })
